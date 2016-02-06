f98e552417bad7186b594a39e6176b7c1876918f
Zanidd <ninijay93@gmail.com>
2016-02-06 22:56:39 +0100

    back to the roots

--- Wingur/Wingur/FrontendController.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class FrontendController
-    Private _picList As List(Of Semantic)
-
-    Public Sub addImage(ByVal imgName As String,
-                        ByVal img As String,
-                        ByVal desc As String)
-
-    End Sub
-End Class
