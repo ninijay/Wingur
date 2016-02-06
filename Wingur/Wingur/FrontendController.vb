6a4ed13a81ea4346d868843556225bcbca9da8f2
Zanidd <ninijay93@gmail.com>
2016-02-06 22:55:29 +0100

    Back to the roots

---
+++ Wingur/Wingur/FrontendController.vb
@@ -0,0 +1,9 @@
+﻿Public Class FrontendController
+    Private _picList As List(Of Semantic)
+
+    Public Sub addImage(ByVal imgName As String,
+                        ByVal img As String,
+                        ByVal desc As String)
+
+    End Sub
+End Class
---
+++ Wingur/wingur-c-sharp/FrontEndController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wingur_c_sharp
+{
+    class FrontEndController
+    {
+        private List<Semantic> list = new List<Semantic>()
+    }
+}
