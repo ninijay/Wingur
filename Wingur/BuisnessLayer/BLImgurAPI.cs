c32f18b50eddf5d8b4f4cc85291baa7e912f481f
nadeaj <james2432@gmail.com>
2016-02-08 18:18:47 -0500

    Base Project
    Base project to build upon. Methods need to be implemented in the
    BusinessLayer and return custom object classes(with processed json/xml)

---
+++ Wingur/BuisnessLayer/BLImgurAPI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wingur
+{
+    class BLImgurAPI
+    {
+        String BaseURL = "https://api.imgur.com/3/";
+
+        public Object GetGallery(String section,String sort, Boolean Viral, Int32 Page)
+        {
+            throw new Exception("Method not implemented yet");
+        }
+
+        public Object GetSubRedditGallery(String Subreddit,String Sort,Int32 Page)
+        {
+            throw new Exception("Method not implemented yet");
+        }
+    }
+}
