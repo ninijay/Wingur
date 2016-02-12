d0c3d6160f13956748cb405c69b613f23f0e802a
Zanidd <ninijay93@gmail.com>
2016-02-12 04:41:07 +0100

    Added API Key for Wingur

--- Wingur/Config/Config.cs
+++ Wingur/Config/Config.cs
@@ -8,7 +8,7 @@ namespace Wingur.Config
 {
     class Config
     {
-        public static string client_id = ""; //TODO: Add client ID (not sharing mine)
-        public static string client_priv = ""; //TODO: Add Client private (not sharing mine)
+        public static string client_id = "4d06b937dbc3d5e"; // Wingur API Key: Without Callback
+        public static string client_priv = "929c4bf703aa4306ef8f9137d50afaffc07c582b"; // Wingur API Key: Without Callback
     }
 }
